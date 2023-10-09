821a3f34b529d62e170787b76a5a6406e78fae13
Danilo <dani320fut@hotmail.com>
2023-10-09 17:41:14 -0300

    fix: error port

--- config/express.js
+++ config/express.js
@@ -3,16 +3,12 @@ const bodyParser = require("body-parser");
 const config = require("config");
 const consign = require("consign");
 const cors = require("cors");
-console.log(
-  "🚀 ~ file: express.js:4 ~ config:",
-  config.get("server.port" || 8080)
-);
 
 module.exports = () => {
   const app = express();
   app.use(cors());
 
-  app.set("port", config.get("server.port"));
+  app.set("port", config.get(8080));
 
   app.use(bodyParser.json());
 
