2f7707ca4797196a20415e09c777c841f36a0fb1
Danilo <dani320fut@hotmail.com>
2023-10-09 19:05:53 -0300

    fix: teste

--- api/routes/articlesBlog.js
+++ api/routes/articlesBlog.js
@@ -1,11 +1,13 @@
 module.exports = (app) => {
-  const {
-    getAllArticlesController,
-    getArticlesBySearchController,
-    getArticlesByPathController,
-  } = app.controllers.articlesBlog;
+  // const {
+  //   getAllArticlesController,
+  //   getArticlesBySearchController,
+  //   getArticlesByPathController,
+  // } = app.controllers.articlesBlog;
 
-  app.route("/api/articles").get(getAllArticlesController);
-  app.route("/api/articles/search").get(getArticlesBySearchController);
-  app.route("/api/articles/path").get(getArticlesByPathController);
+  // app.route("/api/articles").get(getAllArticlesController);
+  // app.route("/api/articles/search").get(getArticlesBySearchController);
+  app.route("/api/").get(() => {
+    return "teste";
+  });
 };
