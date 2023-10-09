05986fd6f17df464a9f5e6ee17212ac4ce03b364
Danilo <dani320fut@hotmail.com>
2023-10-09 18:02:27 -0300

    fix: get

--- api/routes/articlesBlog.js
+++ api/routes/articlesBlog.js
@@ -8,7 +8,4 @@ module.exports = (app) => {
   app.route("/api/articles").get(getAllArticlesController);
   app.route("/api/articles/search").get(getArticlesBySearchController);
   app.route("/api/articles/path").get(getArticlesByPathController);
-  app.route("/").get(() => {
-    return "get test";
-  });
 };
--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const port = app.get("port");
 app.listen(process.env.PORT || port, () => {
   console.log(`rodando na porta ${process.env.PORT || port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
